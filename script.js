f7f20fdc83e4d154bf4d676f810649e0bcda81fe
Shovan001 <mondalshovan28701@gmail.com>
2024-12-12 13:44:56 +0530

    added snake movement

--- script.js
+++ script.js
@@ -5,7 +5,44 @@ document.addEventListener('DOMContentLoaded', function(){
     let score = 0; // score of the ga,e
     let gameStarted = false; // game started or not
     let food = {x: 300, y: 200}; // food position{x:15*20, y:10*20}->cell coordinates ->pixels
-    let snake  = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}]; // snake position  
+    let snake  = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}]; // snake position 
+
+    let dx = cellSize; // snake movement in x direction
+    let dy = 0; // snake movement in y direction
+
+    function updateSnake(){
+        const newHead = {x: snake[0].x + dx, y: snake[0].y + dy};
+        snake.unshift(newHead); // add new head to the snake array
+        if (newHead.x === food.x && newHead.y === food.y){
+            score += 10;
+            
+        }else{
+            snake.pop(); // remove the tail of the snake
+        }
+    }
+
+    function changeDirection(e){
+        console.log("Key pressed", e);
+        const isGoingDown = dy === cellSize;
+        const isGoingUp = dy === -cellSize;
+        const isGoingRight = dx === cellSize;
+        const isGoingLeft = dx === -cellSize;
+        if (e.key === 'ArrowUp'&& !isGoingDown){
+            dx = 0;
+            dy = -cellSize;
+        }else if (e.key === 'ArrowDown' && !isGoingUp){
+            dx = 0;
+            dy = cellSize;
+        }else if (e.key === 'ArrowLeft' && !isGoingRight){
+            dx = -cellSize;
+            dy = 0;
+        }else if (e.key === 'ArrowRight' && !isGoingLeft){
+            dx = cellSize;
+            dy = 0;
+        }
+
+    }
+
 
     function drawDiv(x, y, className){
         const divElement = document.createElement('div');
@@ -27,11 +64,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
     }
 
+    function gameLoop(){
+        setInterval(() => {
+            updateSnake();
+            drawFoodAndSnake();
+        },200)
+    }
+
     function runGame(){
         if(!gameStarted){
             gameStarted = true;
-            drawFoodAndSnake();
-            //gameLoop();
+            document.addEventListener('keydown', changeDirection);
+            gameLoop();
         }
     }
 
